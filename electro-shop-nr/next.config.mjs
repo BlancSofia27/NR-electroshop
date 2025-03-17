670ce46588a9db0aa169e1309fd216320a9bb56a
BlancSofia27 <blancsofia27@gmail.com>
2025-03-17 19:02:00 -0300

    add out

--- electro-shop-nr/next.config.mjs
+++ electro-shop-nr/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ['humjokilfuelljbcfngl.supabase.co'], // Agrega el dominio de Supabase aquí
     }, 
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
   };
   
   export default nextConfig;
